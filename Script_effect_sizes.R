7f3ce795305d2fc5145316f7984990a26490a2e5
cassioalencarnunes <cassioalencarnunes@gmail.com>
2022-02-13 15:13:03 +0000

    Updating the scripts of effect sizes and the relationship with transition rates

--- Script_effect_sizes.R
+++ Script_effect_sizes.R
@@ -1,55 +1,28 @@
-#### Script to calculate the mean effect sizes (R²) and create the graphs ####
+#### Script to calculate the mean effect sizes and create the graphs ####
 
 # Loading packages
 
 library(dplyr)
 library(ggplot2)
-library(boot)
-library(viridis)
 library(lattice)
-library(gridExtra)
 library(tidyverse)
 
 # Loading data
 
-data_R_square <- read.csv("Results/all_results.csv", h=T, row.names = 1)
+efsize_data <- read.csv("efsize_data.csv", h=T)
 
 biodiversity_variables <- c("ant_rich", "bee_rich", "bird_rich", "db_rich", "liana_rich", "sap_rich", "tree_rich")
 carbon_variables <- c("Aboveground_pool", "Litter_pool", "Dead_pool", "Soil_pool_new")
 soil_variables <- c("pH", "Nperc", "P", "K", "Na", "CaMg", "Al")
 
-data_R_square[data_R_square$variable %in% biodiversity_variables,"Component"] <- "biodiversity"
-data_R_square[data_R_square$variable %in% carbon_variables,"Component"] <- "carbon" 
-data_R_square[data_R_square$variable %in% soil_variables,"Component"] <- "soil"
-
-
-#### Bootstrapping the Confidence interval of the mean for R-squared ####
-
-# Function to obtain the mean
-Bmean <- function(data, indices) {
-  d <- data[indices]
-  return(mean(d))
-} 
-
-# Function to obtain the CI of the mean
-
-boot_ci_nunes <- function(data, n_random, type){
-  results <- boot(data = data, statistic = Bmean, R = n_random)
-  ci <- boot.ci(results, type = type)
-  tibble(ci_lb = ci$bca[4], ci_ub = ci$bca[5])
-}
-
-average_R2 <- data_R_square %>% 
-  group_by(Component, Transition) %>% 
-  summarise(mean_R2 = mean(R2m), boot_ci_nunes(data = R2m, n_random = 10000, type = "bca"),
-            n = n())
-
-write.csv(file = "Results/average_R2.csv", x = average_R2)
+efsize_data[efsize_data$variable %in% biodiversity_variables,"Component"] <- "biodiversity"
+efsize_data[efsize_data$variable %in% carbon_variables,"Component"] <- "carbon" 
+efsize_data[efsize_data$variable %in% soil_variables,"Component"] <- "soil"
 
 
 #### Graphs ####
 
-## Mean effect sizes ##
+## Effect sizes ##
 
 #My APA-format theme
 apatheme=theme_bw()+
@@ -60,53 +33,69 @@ apatheme=theme_bw()+
         text=element_text(family='Times'),
         legend.position='none')
 
-# Plot Data Frame
-average_R2_ordered <- average_R2 %>%
+efsize_data_ordered <- efsize_data %>%
+  group_by(Component, transition) %>% 
+  summarise(median_es = median(abs(Estimate)))
+
+efsize_data_ordered <- efsize_data_ordered %>% 
   ungroup() %>% 
-  # 2. Arrange by
-  #   i.  facet group
-  #   ii. bar height
-  arrange(Component, mean_R2) %>%
-  # 3. Add order column of row numbers
+  arrange(Component, median_es) %>% 
   mutate(order = row_number())
 
+efsize_data2 <- efsize_data %>% 
+  group_by(Component) %>% 
+  left_join(efsize_data_ordered, by = c("Component", "transition"))
 
-ggplot(average_R2_ordered, aes(y=order, x=mean_R2, xmin=ci_lb, xmax=ci_ub, colour = factor(Component)))+
-  geom_point(size=3)+
-  geom_errorbarh(aes(colour = factor(Component)), size = 1, height=1)+
+#write.csv(x = efsize_data_ordered, file = "efsize_median.csv", row.names = F)
+
+
+# Plot
+
+ggplot(efsize_data2)+
+  geom_boxplot(aes(y=as.factor(order), x=abs(Estimate)), fill=NA, outlier.shape = NA)+
+  geom_text(aes(y=as.factor(order), x=2.8, label = round(median_es, digits = 2)))+
+  geom_point(aes(y=as.factor(order), x=abs(Estimate), colour = factor(Component)),
+             size=3, alpha = 0.65)+
   scale_colour_manual(values = c("#006D2C", "#F57D15FF", "#781C6D"))+
-  scale_x_continuous(limits = c(0,1), name='R²')+
-  facet_grid(Component~., scales = 'free', space='free')+
-  geom_vline(xintercept=0)+
-  scale_y_continuous(
-    breaks = average_R2_ordered$order,
-    labels = average_R2_ordered$Transition,
-    expand = c(0,0)
+  facet_grid(Component~., scales= 'free', space='free')+
+  scale_y_discrete(
+    breaks = as.numeric(efsize_data_ordered$order),
+    labels = as.factor(efsize_data_ordered$transition),
+    expand = c(0.05,0)
   )+
-  #Apply my APA theme
   apatheme
+
+
 
 
 ## Heat map
 
-data_all_joined <- read.csv("Data/data_all_joined.csv", h=T, row.names = 1)
+efsize_data <- read.csv("efsize_data.csv", h=T)
 
-data_R_square <- data_R_square[order(match(data_R_square$variable,
-                                           c("tree_rich", "sap_rich", "liana_rich", "bird_rich", "ant_rich", "db_rich",
-                                             "bee_rich", "Aboveground_pool", "Litter_pool", "Dead_pool", "Soil_pool_new",
-                                             "pH", "Al", "CaMg", "K", "Nperc", "Na", "P"))),]
+efsize_data <- efsize_data[order(match(efsize_data$variable,
+                                       c("tree_rich", "sap_rich", "liana_rich", "bird_rich", "ant_rich", "db_rich",
+                                         "bee_rich", "Aboveground_pool", "Litter_pool", "Dead_pool", "Soil_pool_new",
+                                         "pH", "Al", "CaMg", "K", "Nperc", "Na", "P"))),]
 
-data_R_square <- data_R_square[order(match(data_R_square$Transition,
-                                           c("UF_LF", "UF_LBF", "UF_PA", "UF_MA", "LF_LBF", "LF_PA", "LF_MA", 
-                                             "LBF_PA", "LBF_MA", "PA_MA", "MA_SFyoung", "PA_SFyoung",
-                                             "SFyoung_SFold", "SFold_PA", "SFold_MA"))),]
+efsize_data <- efsize_data[order(match(efsize_data$transition,
+                                       c("UF_LF", "UF_LBF", "UF_PA", "UF_MA", "LF_LBF", "LF_PA", "LF_MA", 
+                                         "LBF_PA", "LBF_MA", "PA_MA", "MA_SFyoung", "PA_SFyoung",
+                                         "SFyoung_SFold", "SFold_PA", "SFold_MA"))),]
 
 
-matrix <- as.data.frame(pivot_wider(data_R_square, id_cols = Transition, names_from = variable, values_from = R2m))
-row.names(matrix) <- matrix$Transition
+matrix <- as.data.frame(pivot_wider(efsize_data, id_cols = transition, names_from = variable, values_from = Estimate))
+row.names(matrix) <- matrix$transition
 
 levelplot(t(as.matrix(matrix[,2:19])), col.regions = colorRampPalette(c("#FCFDBFFF" ,"#FEBA80FF", "#F8765CFF", "#D3436EFF", "#982D80FF", "#5F187FFF", "#180F3EFF"))(200),
-          colorkey = list(TRUE, height = 1), at = seq(0,1,0.01), scales = list(y = (list(cex=1, font=2)), tck = c(1,0), x=list(cex=1, rot=45, font=2)),
+          colorkey = list(TRUE, height = 1), at = seq(-1.8,2.7,0.05), scales = list(y = (list(cex=1, font=2)), tck = c(1,0), x=list(cex=1, rot=45, font=2)),
           aspect = 15/19, xlab = NULL, ylab = "Transitions")
 
+levelplot(t(as.matrix(matrix[,2:19]))*-1, col.regions = colorRampPalette(c("#01665e", "#35978f", "#5ab4ac", "#f5f5f5", "#dfc27d", "#d8b365", "#8c510a"))(200),
+          colorkey = list(TRUE, height = 1), at = seq(-2.7,2.7,0.05), scales = list(y = (list(cex=1, font=2)), tck = c(1,0), x=list(cex=1, rot=45, font=2)),
+          aspect = 15/19, xlab = NULL, ylab = "Transitions")
 
+#d8b365
+#f5f5f5
+#5ab4ac
+
+range(matrix[,2:19])
--- Script_relationship_LUCT.R
+++ Script_relationship_LUCT.R
@@ -5,13 +5,22 @@
 library(dplyr)
 library(ggplot2)
 library(viridis)
+library(DHARMa)
 
 # Loading data
 
-data_R2 <- read.csv("Results/average_R2.csv", h=T, row.names = 1)
+data_ef <- read.csv("efsize_median.csv", h=T)
 data_transitions <- read.table("Data/Transition_rates.txt", h=T)
 
-data_all <- left_join(x = data_R2, y = data_transitions, by = "Transition")
+data_transitions[!data_transitions$transition %in% data_ef$transition, "transition"]
+
+#Repeating data for bidirectional transitions: "SFyoung_MA", "MA_PA" and "SFyoung_PA"
+
+data_ef <- rbind(data_ef, data_ef[data_ef$transition %in% c("MA_SFyoung", "PA_MA", "PA_SFyoung"),])
+
+data_ef[(54-8):54,"transition"] <- rep(c("MA_PA","SFyoung_PA", "SFyoung_MA"), 3)
+
+data_all <- left_join(x = data_ef, y = data_transitions, by = "transition")
 
 
 biodiversity <- droplevels(data_all[data_all$Component=="biodiversity",])
@@ -20,11 +29,20 @@ soil <- droplevels(data_all[data_all$Component=="soil",])
 
 #### Testing the regression models ####
 
-anova(lm(mean_R2~log(km2), data = biodiversity), test = "F")
-anova(lm(mean_R2~log(km2), data = carbon), test = "F")
-anova(lm(mean_R2~log(km2), data = soil))
-  
-# None is statistically significant!
+# Checking normality of effect sizes and log of transition's rates
+
+shapiro.test(log(biodiversity$km2))
+shapiro.test(biodiversity$median_es) # OK
+shapiro.test(carbon$median_es) # No, spearman instead of Pearson
+shapiro.test(soil$median_es) # OK
+
+cor.test(y=biodiversity$median_es, x=log(biodiversity$km2))
+cor.test(y=carbon$median_es, x=log(carbon$km2), method = "spearman")
+cor.test(y=soil$median_es, x=log(soil$km2))
+
+# There is a negative correlation between transition rate and effect size for
+# biodiversity and soil, but not for carbon
+
 
 ## Creating the graph
 
@@ -38,37 +56,37 @@ apatheme=theme_bw()+
         legend.position='none')
 
 
-  # Calculating the medians - Separating the quadrants: i) low impact, low rate, 
-      # ii) low impact, high rate, iii) high impact, low-rate, iv) high impact, high rate.
+# Calculating the medians - Separating the quadrants: i) low impact, low rate, 
+# ii) low impact, high rate, iii) high impact, low-rate, iv) high impact, high rate.
 
-  vline.data <- data.frame(data_all %>% 
-                             group_by(Component) %>% 
-                             summarise(median(log(km2))))
-  names(vline.data)[2] <- "median_km2"
+vline.data <- data.frame(data_all %>% 
+                           group_by(Component) %>% 
+                           summarise(median(log(km2))))
+names(vline.data)[2] <- "median_km2"
 
-  hline.data <- data.frame(data_all %>% 
-                             group_by(Component) %>% 
-                             summarise(median(mean_R2)))
-  names(hline.data)[2] <- "median_R2"
+hline.data <- data.frame(data_all %>% 
+                           group_by(Component) %>% 
+                           summarise(median(median_es)))
+names(hline.data)[2] <- "median_es"
 
 
 # Plotting the graph
-  
-ggplot(data_all, aes(y=mean_R2, x=log(km2), colour = factor(Component)))+
+
+ggplot(data_all, aes(y=median_es, x=log(km2), colour = factor(Component)))+
   #Add data points and color them black
   geom_point(size = 4)+
   geom_vline(aes(xintercept = median_km2), vline.data)+
-  geom_hline(aes(yintercept = median_R2), hline.data)+
-  scale_x_continuous(name='log (km²/year)')+
+  geom_hline(aes(yintercept = median_es), hline.data)+
+  scale_x_continuous(name='log (km?/year)')+
   scale_colour_manual(values = c("#006D2C", "#F57D15", "#781C6D"))+
-  ylim(c(0,1))+
+  ylim(c(0,2.8))+
   #labels of points
-  geom_text(label = data_all$Transition, 
+  geom_text(label = data_all$transition, 
             nudge_x = 0.030,
             nudge_y = 0.045,
             check_overlap = F, colour = "black")+
   #Give y-axis a meaningful label
-  ylab('R²')+
+  ylab('Effect size')+
   #Create sub-plots (i.e., facets) based on levels of setting
   #And allow them to have their own unique axes (so authors don't redundantly repeat)
   facet_grid(cols = vars(Component), scales= 'fixed', space='free')+
